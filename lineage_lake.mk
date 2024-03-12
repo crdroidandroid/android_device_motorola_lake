c8042215e25162f90310af15211b15a0f404b924
skullknight963 <viktor.9630@hotmail.com>
2024-03-12 06:33:10 -0300

    lake: Remove build fingerprint to makefile
    * We will use da source for certification.
    
    * Correct Product.
    
    Change-Id: I8612058b16379686d9ee61247991c60790ebd50b
    Signed-off-by: skullknight963 <viktor.9630@hotmail.com>

--- lineage_lake.mk
+++ lineage_lake.mk
@@ -7,13 +7,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/lake/device.mk)
 
 # Device identifiers
-BUILD_FINGERPRINT := motorola/lake_retail/lake:10/QPWS30.61-21-18-7-12/b1002:user/release-keys
-PRODUCT_BRAND := motorola
+PRODUCT_BRAND := Motorola
 PRODUCT_DEVICE := lake
-PRODUCT_MANUFACTURER := motorola
-PRODUCT_MODEL := moto g(7) plus
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_MODEL := Moto G7 Plus
 PRODUCT_NAME := lineage_lake
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=lake \
-    PRIVATE_BUILD_DESC="lake-user 10 QPWS30.61-21-18-7-12 b1002 release-keys"
+    PRODUCT_NAME=lake
